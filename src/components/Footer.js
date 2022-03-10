ae2a9a5dd198c2cb893239f60fe8228496017bdc
Will Haberer <willhaberer@gmail.com>
2022-03-10 14:07:49 -0500

    added logos to footer and styled logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import ghLogo from "../assets/ghLogo.png";
+import liLogo from "../assets/LiLogo.png";
+import emailLogo from "../assets/emailLogo.png";
+
 import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div id="footer">Hello</div>;
+  return (
+    <div id="footer">
+      <h3 id="footerContact">Contact</h3>
+      <div id="contactLinks">
+        <a
+          href="https://github.com/willhaberer"
+          target="_blank"
+          rel="noreferrer"
+          id="contactLogo"
+        >
+          <img src={ghLogo} alt="Github Logo" height="8%" width="8%"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/will-haberer/"
+          target="_blank"
+          rel="noreferrer"
+          id="contactLogo"
+        >
+          <img src={liLogo} alt="Linkedin Logo" height="8%" width="8%"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/will-haberer/"
+          target="_blank"
+          rel="noreferrer"
+          id="contactLogo"
+        >
+          <img src={emailLogo} alt="Email Logo" height="8%" width="8%"></img>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
