7f5a84c41ce413424c1e2224faf9a2b3c7e36253
Will Haberer <willhaberer@gmail.com>
2022-03-09 20:06:16 -0500

    added typescript effect to name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TypeAnimation from "react-type-animation";
 
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  return <div id="navbar">Navbar</div>;
+  return (
+    <div id="navbar">
+      <TypeAnimation
+        cursor={true}
+        sequence={["Will Haberer", 1000, "Will Haberer"]}
+        wrapper="h2"
+        repeat={1}
+      />
+      <Link id="link" to="/about">
+        About
+      </Link>
+      <Link id="link" to="/portfolio">
+        Portfolio
+      </Link>
+      <Link id="link" to="/contact">
+        Contact
+      </Link>
+      <Link id="link" to="/resume">
+        Resume
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
