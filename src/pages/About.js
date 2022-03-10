b92db45128b90496fc76a87a4ac8a9cb212874d9
Will Haberer <willhaberer@gmail.com>
2022-03-10 15:34:38 -0500

    built out front end skills, added zoom effect on hovering a skill

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,10 @@ import "../styles/About.css";
 
 import html5Logo from "../assets/html5Logo.png";
 import cssLogo from "../assets/cssLogo.png";
+import javascriptLogo from "../assets/javascriptLogo.png";
+import jqueryLogo from "../assets/jqueryLogo.png";
+import reactLogo from "../assets/reactLogo.png";
+import bootstrapLogo from "../assets/bootstrapLogo.png";
 
 function About() {
   return (
@@ -33,6 +37,46 @@ function About() {
                 id="skillLogo"
               ></img>
               <h5 id="caption">CSS</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={javascriptLogo}
+                alt="Javascript Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Javascript</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={jqueryLogo}
+                alt="Jquery Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">jQuery</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={reactLogo}
+                alt="React Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">React</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={bootstrapLogo}
+                alt="Bootstrap Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Bootstrap</h5>
             </li>
           </ul>
         </div>
