abffff4ca55d97aab25ae705d6dce54cfd733ce7
Will Haberer <willhaberer@gmail.com>
2022-03-09 19:10:13 -0500

    created home page and navbar compenent and installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Particles from "react-tsparticles";
 
 import "./styles/App.css";
 import particlesOptions from "./particles.json";
 
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <Particles options={particlesOptions} />
+      <BrowserRouter>
+        <Navbar />
+        <div id="body">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <div id="navbar">Navbar</div>;
+};
+
+export default Navbar;
