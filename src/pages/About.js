3e7f621578586ae7e7607b56439932402dca2d37
Will Haberer <willhaberer@gmail.com>
2022-03-10 16:13:58 -0500

    added backend skills to skills list

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,6 +8,13 @@ import javascriptLogo from "../assets/javascriptLogo.png";
 import jqueryLogo from "../assets/jqueryLogo.png";
 import reactLogo from "../assets/reactLogo.png";
 import bootstrapLogo from "../assets/bootstrapLogo.png";
+import nodeLogo from "../assets/nodeLogo.png";
+import expressLogo from "../assets/expressLogo.png";
+import graphLogo from "../assets/graphLogo.png";
+import mySqlLogo from "../assets/mySqlLogo.png";
+import mongoLogo from "../assets/mongoLogo.png";
+import seqLogo from "../assets/sequelizeLogo.png";
+import jestLogo from "../assets/jestLogo.png";
 
 function About() {
   return (
@@ -77,6 +84,81 @@ function About() {
                 id="skillLogo"
               ></img>
               <h5 id="caption">Bootstrap</h5>
+            </li>
+          </ul>
+        </div>
+        <h5 id="aboutSubHeader">Back End</h5>
+        <div id="backEnd">
+          <ul id="card-wrapper">
+            <li id="skillCard">
+              <img
+                src={nodeLogo}
+                alt="Node Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Node Js</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={expressLogo}
+                alt="Express Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Expres Js</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={graphLogo}
+                alt="Graph Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">GraphQL</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={mongoLogo}
+                alt="MongoDB Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">MongoDB</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={mySqlLogo}
+                alt="MySQL Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">MySQL</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={seqLogo}
+                alt="MySQL Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Sequelize</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={jestLogo}
+                alt="Jest Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">Jest</h5>
             </li>
           </ul>
         </div>
