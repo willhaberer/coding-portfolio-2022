5eefbfeaaac8532bd2a346d482b6d53a755931af
Will Haberer <willhaberer@gmail.com>
2022-03-10 15:13:17 -0500

    added styles to skill cards, added first two skill cards

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,40 @@ import React from "react";
 
 import "../styles/About.css";
 
+import html5Logo from "../assets/html5Logo.png";
+import cssLogo from "../assets/cssLogo.png";
+
 function About() {
   return (
     <div id="mainContainer">
       <h1>About</h1>
       <div id="skillsContainer">
         <h3 id="aboutHeader">Skills</h3>
+        <h5 id="aboutSubHeader">Front End</h5>
+        <div id="frontEnd">
+          <ul id="card-wrapper">
+            <li id="skillCard">
+              <img
+                src={html5Logo}
+                alt="HTML5 Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">HTML5</h5>
+            </li>
+            <li id="skillCard">
+              <img
+                src={cssLogo}
+                alt="CSS Logo"
+                height="50%"
+                width="50%"
+                id="skillLogo"
+              ></img>
+              <h5 id="caption">CSS</h5>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
