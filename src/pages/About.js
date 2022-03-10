c3a5126c9939fdb84ec54614bd1c78acb79141a2
Will Haberer <willhaberer@gmail.com>
2022-03-10 14:27:37 -0500

    added header to about page, updated styles of footer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import "../styles/About.css";
+
 function About() {
   return (
     <div id="mainContainer">
       <h1>About</h1>
+      <div id="skillsContainer">
+        <h3 id="aboutHeader">Skills</h3>
+      </div>
     </div>
   );
 }
