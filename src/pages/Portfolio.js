f37b46999266350208d472739d9ba7491003c63c
Will Haberer <willhaberer@gmail.com>
2022-03-11 19:24:52 -0500

    finished portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,6 +12,7 @@ import mernLogo from "../assets/mernLogo.png";
 import webApi from "../assets/webAPIS.png";
 import mysqlLogo from "../assets/mySqlLogo.png";
 import expressLogo from "../assets/expressLogo.png";
+import handlebarsLogo from "../assets/handlebars.png";
 
 import "../styles/Portfolio.css";
 
@@ -33,7 +34,58 @@ function Portfolio() {
             a relatively low amount of streams. FMSN also allows users to save
             these songs to their profile to listen again later.
           </p>
-
+          <ul id="porCard-wrapper">
+            <li id="porSkillCard">
+              <img
+                src={html5Logo}
+                alt="HTML5 Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">HTML5</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={cssLogo}
+                alt="CSS Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">CSS</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={javascriptLogo}
+                alt="Javascript Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Javascript</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={mernLogo}
+                alt="MERN Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">MERN</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={webApi}
+                alt="Web API Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Web APIs</h5>
+            </li>
+          </ul>
           <div id="projectLinks">
             <a
               href="https://github.com/willhaberer/Find-Me-Something-New"
@@ -73,8 +125,73 @@ function Portfolio() {
         <img class="porImage__img" src={ftnssjnks} alt="Ftnss Jnks"></img>
         <div class="porImage__overlay porImage__overlay--blur">
           <div class="porImage__title">Ftnss Jnks</div>
-          <p class="porImage__description">Here we have a bri.</p>
-
+          <p class="porImage__description">
+            Ftnss Jnks (pronounced Fitness Junkies) is a one stop shop that
+            allows users to plan a workout routine and find recipe's that will
+            support their fitness goals.
+          </p>
+          <ul id="porCard-wrapper">
+            <li id="porSkillCard">
+              <img
+                src={html5Logo}
+                alt="HTML5 Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">HTML5</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={cssLogo}
+                alt="CSS Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">CSS</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={javascriptLogo}
+                alt="Javascript Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Javascript</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={mysqlLogo}
+                alt="MySQL Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">MySQL</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={expressLogo}
+                alt="Express Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Express</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={handlebarsLogo}
+                alt="Handlebars Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Handlebars</h5>
+            </li>
+          </ul>
           <div id="projectLinks">
             <a
               href="https://github.com/hyanez/Ftnss-Jnks"
@@ -108,12 +225,57 @@ function Portfolio() {
         </div>
       </div>
       <br></br>
-      <h3 id="projectName">Full Stack</h3>
+      <h3 id="projectName">Front End</h3>
       <div class="porImage" id="bottomImage">
         <img class="porImage__img" src={wd} alt="Weather Dash"></img>
         <div class="porImage__overlay porImage__overlay--blur">
           <div class="porImage__title">Weather Dashboard</div>
-          <p class="porImage__description">Here we have a brick wall.</p>
+          <p class="porImage__description">
+            This weather dashboard allows users to search for a city and receive
+            the current and 5 day forecasts.
+          </p>
+          <ul id="porCard-wrapper">
+            <li id="porSkillCard">
+              <img
+                src={html5Logo}
+                alt="HTML5 Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">HTML5</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={cssLogo}
+                alt="CSS Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">CSS</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={javascriptLogo}
+                alt="Javascript Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Javascript</h5>
+            </li>
+            <li id="porSkillCard">
+              <img
+                src={webApi}
+                alt="Web API Logo"
+                height="50%"
+                width="50%"
+                id="porSkillLogo"
+              ></img>
+              <h5 id="porCaption">Web APIs</h5>
+            </li>
+          </ul>
           <div id="projectLinks">
             <a
               href="https://github.com/willhaberer/Weather-Dashboard"
