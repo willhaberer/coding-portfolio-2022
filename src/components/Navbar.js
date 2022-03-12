eeb05b395d8d6a2ef0914c88ab924283ddeb885e
Will Haberer <willhaberer@gmail.com>
2022-03-11 19:50:45 -0500

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,28 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <div id="navbar">
-      <TypeAnimation
-        cursor={true}
-        sequence={["Will Haberer", 1000, "Will Haberer"]}
-        wrapper="h2"
-        repeat={1}
-      />
-      <Link id="link" to="/about">
-        About
-      </Link>
-      <Link id="link" to="/portfolio">
-        Portfolio
-      </Link>
-      <Link id="link" to="/contact">
-        Contact
-      </Link>
-      <Link id="link" to="/resume">
-        Resume
-      </Link>
+      <div id="siteName">
+        <TypeAnimation
+          cursor={true}
+          sequence={["Will Haberer Portfolio", 1000, "Will Haberer"]}
+          wrapper="h2"
+          repeat={1}
+        />
+      </div>
+      <div id="navList">
+        <Link id="link" to="/about">
+          About
+        </Link>
+        <Link id="link" to="/portfolio">
+          Portfolio
+        </Link>
+        <Link id="link" to="/contact">
+          Contact
+        </Link>
+        <Link id="link" to="/resume">
+          Resume
+        </Link>
+      </div>
     </div>
   );
 };
