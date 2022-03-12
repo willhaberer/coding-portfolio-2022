470e8cea96e051be38801ad788c7b6cd05b2218a
Will Haberer <willhaberer@gmail.com>
2022-03-11 21:31:21 -0500

    updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import "../styles/Home.css";
+
 function Home() {
   return (
-    <div id="mainContainer">
-      <h1>Home</h1>
+    <div id="homeContainer">
+      <h1 id="homeHeader">I'm Will, Welcome to My Portfolio</h1>
+      <p id="homeP">
+        I'm excited to start a career in Full-Stack Web Development where I can
+        translate my passion for creating and solving logic based puzzles into a
+        career.
+      </p>
+      <p id="homeBottomP">
+        Feel free to reach out to me at: willhaberer@gmail.com
+      </p>
     </div>
   );
 }
