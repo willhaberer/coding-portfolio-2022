0c1ed9a46f2d7884d5d73dd20942167923b3ea85
Will Haberer <willhaberer@gmail.com>
2022-03-11 21:04:06 -0500

    reordered nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,11 +24,11 @@ const Navbar = () => {
         <Link id="link" to="/portfolio">
           Portfolio
         </Link>
-        <Link id="link" to="/contact">
-          Contact
-        </Link>
         <Link id="link" to="/resume">
           Resume
+        </Link>
+        <Link id="link" to="/contact">
+          Contact
         </Link>
       </div>
     </div>
